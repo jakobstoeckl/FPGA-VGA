fc6072d393dd56fa2d053e5742d8735fff0e5e14
tuabu <jakob.stoeckl@gmail.com>
2021-10-10 14:43:46 +0200

    Implementing delete functionality
    This modification takes care of deleting the previous character
    from the display ram. It also takes into account to deal with
    the addr_counter (addr of ram when writing).

--- design/top.v
+++ design/top.v
@@ -18,11 +18,12 @@ wire gcb_clk;
 wire w_valid_area;
 wire w_px;
 
-reg [11:0] addr_counter = 12'b000000000000;
+reg [11:0] addr_counter = 12'b00000000001;
 reg  r_button = 1'b0;
 wire w_button;
 
 reg [7:0] r_i_data = 8'b0;
+reg [7:0] r_pre_data = 8'b0;
 reg r_write_flag = 1'b0;
 reg r_corruption_flag = 1'b0;
 wire w_h_sync;
@@ -41,45 +42,61 @@ debounce deb(
     .i_Switch(P13),
     .o_Switch(w_button)
     );
-
+//Main trigger
 always @(posedge gcb_clk)
     begin
+      //creates previous & next state for input we
       r_button <= w_button;
-      if (w_button == 1'b1 && r_button == 1'b0)
+      //checks for falling edge input we
+      if (w_button == 1'b0 && r_button == 1'b1)
         begin
-          r_i_data <= P;
-          if(w_invert_h_sync == 1'b0)
-            begin
-              r_write_flag <= 1'b1;
-              if(addr_counter == 12'b100101100000)
-                begin
-                  addr_counter <= 12'b000000000000;
-                end
-              else
-                begin
-                  addr_counter <= addr_counter + 1;
-                end
-            end
-          else
-            begin
-              r_corruption_flag <= 1'b1;
-              r_write_flag <= 1'b0;
-            end
+            //if previous command was delete and actual command is delete treate addr_counter right
+            if(r_pre_data == 8'h00 && P == 8'h7F)
+              begin
+                r_i_data <= 8'b00000000;
+                r_pre_data <= r_i_data;
+                //TODO: Boundry for addr_counter
+                addr_counter <= addr_counter - 1;
+              end
+            //if actual command is delete
+            else if(P == 8'h7F)
+              begin
+                r_i_data <= 8'b00000000;
+                r_pre_data <= r_i_data;
+              end
+            //if actual command is not delete but previous was
+            else if(r_pre_data == 8'h00 && ~(P == 8'h7F))
+              begin
+                r_i_data <= P;
+                r_pre_data <= r_i_data;
+              end
+            //if actual command is not delete
+            else
+              begin
+                r_i_data <= P; //sample input data
+                r_pre_data <= r_i_data;
+                //TODO: add boundry for addr_counter
+                addr_counter <= addr_counter + 1;
+              end
+            if(w_invert_h_sync == 1'b0) //if we aren't in the sync pulse throw write flag
+              begin
+                r_write_flag <= 1'b1;
+              end
+            else                       //if we are in the sync pulse throw corruption_flag
+              begin
+                r_corruption_flag <= 1'b1;
+              end
+      end //if button
+      else if(w_invert_h_sync == 1'b1)  // clear write flag on every sync pulse
+      begin
+        r_write_flag <= 1'b0;
       end
-      if(r_corruption_flag == 1'b1 && w_invert_h_sync == 1'b0)
+      if(r_corruption_flag == 1'b1 && w_invert_h_sync == 1'b0) //if previous the corruption_flag was thrown now take care of it
         begin
           r_corruption_flag <= 1'b0;
           r_write_flag <= 1'b1;
-          if(addr_counter == 12'b100101100000)
-            begin
-              addr_counter <= 12'b000000000000;
-            end
-          else
-            begin
-              addr_counter <= addr_counter + 1;
-            end
-        end
-    end
+      end
+end//always
 
 vga vga_inst(
     .i_clk(gcb_clk),
